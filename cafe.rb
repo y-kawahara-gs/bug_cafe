b7c0ca122b065ae4cc03f7826b04175d540a5f1b
y-kawahara-gs <y.kawahara@genbasupport.com>
2025-08-04 08:22:15 +0900

    各訂正ポイントの修正

--- cafe.rb
+++ cafe.rb
@@ -14,26 +14,21 @@ FOODS = [
   { name: 'ホットサンド', price: '410' }
 ].freeze
 
-
 def take_order(menus)
   menus.each.with_index(1) do |menu, i|
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
   print '>'
-  # 配列の要素数は0がスタートなので、-1を追加
   order_number = gets.to_i - 1
   puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drink = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+food = take_order(FOODS)
 
-
-#DRINKSのtake_orderを管理しているorder1とFOODSのtake_orderを管理しているorder2が逆になっているため修正
-#また、値段が文字列になっているため数値に変換
-total =DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+total = DRINKS[drink][:price].to_i + FOODS[food][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
